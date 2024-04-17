b8fa15922bc38cc3e8daeef33ae17bff96de718a
abdulllkhan <abdulskhan47@gmail.com>
2024-04-17 03:00:40 -0400

    Simple Hello World API running

--- src/main/java/com/project/stone/StoneApplication.java
+++ src/main/java/com/project/stone/StoneApplication.java
@@ -2,8 +2,10 @@ package com.project.stone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.project.stone", "com.project.stone.hello"})
 public class StoneApplication {
 
 	public static void main(String[] args) {
