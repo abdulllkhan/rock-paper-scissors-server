b12c53fa37c95cecc819176cf1d8dc73e0a9c1cf
abdulllkhan <abdulskhan47@gmail.com>
2024-04-25 23:26:49 -0400

    Incomplete websocket implementation. Working on password salt/pepper security

--- src/main/java/com/project/stone/StoneApplication.java
+++ src/main/java/com/project/stone/StoneApplication.java
@@ -3,8 +3,10 @@ package com.project.stone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 @ComponentScan(basePackages = {"com.project.stone", "com.project.stone.hello", "com.project.stone.user"})
 public class StoneApplication {
 
---
+++ src/main/java/com/project/stone/websocket/MyWebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.project.stone.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+@Component
+public class MyWebSocketHandler implements WebSocketHandler {
+
+    private final WebSocketSessions webSocketSessions;
+
+    @Autowired
+    public MyWebSocketHandler(WebSocketSessions webSocketSessions) {
+        this.webSocketSessions = webSocketSessions;
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        webSocketSessions.addSession(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        webSocketSessions.removeSession(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleMessage'");
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleTransportError'");
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+}
